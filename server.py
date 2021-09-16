0fa4e2ef5118c477813b7e17249ca37df4e5a2b3
lucaspgraciano <lucaspgraciano@hotmail.com>
2021-09-16 18:53:30 -0300

    [FEATURE] Rota para a template index.html

--- server.py
+++ server.py
@@ -1,19 +1,25 @@
 from flask import Flask
 from flask import render_template
 
+
 app = Flask(__name__)
 
 @app.route("/")
-def home(name=None):
-    return render_template('login.html', name=name)
+def home():
+    return render_template('login.html')
 
 @app.route("/login.html")
-def login(name=None):
-    return render_template('login.html', name=name)
+def login():
+    return render_template('login.html')
 
 @app.route("/register.html")
-def register(name=None):
-    return render_template('register.html', name=name)
+def register():
+    return render_template('register.html')
+
+@app.route("/index.html")
+def index():
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
