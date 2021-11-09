bbc6c5fdb75d00a9a2354c807dabad57dcbbae40
lucaspgraciano <lucaspgraciano@hotmail.com>
2021-11-09 15:40:29 -0300

    [STYLE|CHANGES] Inseri novos svg na home e alterei o formulário da primeira lista

--- app/view/user/home.php
+++ app/view/user/home.php
@@ -36,6 +36,17 @@
 				</header>
 			</div>
 			<main class="main-content">
+				<?php if (is_null($data[1]) || count($data[1]) === 0) { ?>
+					<form action="/user/home/new_list" method="POST">
+						<div class="main-content-first_list">
+							<h1>Ops, não encontramos nenhuma lista em seu usuário</h1>
+							<p> Vamos começar! Por favor, insira o título desejado para sua lista e clique em adicionar</p>
+							<input name="titulo" id="titulo" type="text" placeholder="Lista...">
+							<button type="submit">Adicionar</button>
+							<img src="../../../public/images/reading.svg" class="home-no-task_bg-svg">
+						</div>
+					</form>
+				<?php } else { ?>
 				<section id="Listas" class="main-content-list">
 					<div class="main-content-list_user-list">
 						<section>
@@ -49,59 +60,57 @@
 						</section>
 					</div>
 					<div class="main-content-list_user-list">
-						<?php if (is_null($data[1]) || count($data[1]) === 0) { ?>
-						<?php } else {
-							foreach ($data[1] as $lista) { ?>
-								<section>
-									<header>
-										<div>
-											<form action="/user/home/remove_list" method="POST">
-												<input name="titulo" id="titulo" type="hidden" value="<?= $lista->titulo ?>">
-												<div>
-													<label><?= $lista->titulo ?></label>
-													<button type="submit"> Excluir </button>
-												</div>
-											</form>
-										</div>
-										<form action="/user/home/add_task" method="POST">
-											<input name="conteudo" id="conteudo" type="text" placeholder="Tarefa...">
+						<?php foreach ($data[1] as $lista) { ?>
+							<section>
+								<header>
+									<div>
+										<form action="/user/home/remove_list" method="POST">
 											<input name="titulo" id="titulo" type="hidden" value="<?= $lista->titulo ?>">
-											<button type="submit">Add</button>
+											<div>
+												<label><?= $lista->titulo ?></label>
+												<button type="submit"> Excluir </button>
+											</div>
 										</form>
-									</header>
-									<main>
-										<?php if (is_null($data[2]) || count($data[2]) === 0) { ?>
-										<?php } else {
-											foreach ($data[2] as $tarefa) { 
-												if ($tarefa->titulo == $lista->titulo) { ?>
-													<div>
-														<form action="/user/home/remove_task" method="POST">
-															<input name="conteudo" id="conteudo" type="hidden" value="<?= $tarefa->conteudo ?>">
-															<input name="titulo" id="titulo" type="hidden" value="<?= $tarefa->titulo ?>">
-															<label><?= $tarefa->conteudo ?></labe>
-															
-															<button type="submit"> Exluir </button>
-														</form>
-													</div>
-													<section>
-														<form action="/user/home/update_task" method="POST">
-															<input name="conteudo" id="conteudo" type="hidden" value="<?= $tarefa->conteudo ?>">
-															<input name="titulo" id="titulo" type="hidden" value="<?= $tarefa->titulo ?>">
-															<input name="estado" id="estado" type="hidden" value="<?= $tarefa->estado ?>">
-															
-															<button type="submit"><?= $tarefa->estado ?></button>
-														</form>
-													</section>
-												<?php } ?>
+									</div>
+									<form action="/user/home/add_task" method="POST">
+										<input name="conteudo" id="conteudo" type="text" placeholder="Tarefa...">
+										<input name="titulo" id="titulo" type="hidden" value="<?= $lista->titulo ?>">
+										<button type="submit">Add</button>
+									</form>
+								</header>
+								<main>
+									<?php if (is_null($data[2]) || count($data[2]) === 0) { ?>
+									<?php } else {
+										foreach ($data[2] as $tarefa) { 
+											if ($tarefa->titulo == $lista->titulo) { ?>
+												<div>
+													<form action="/user/home/remove_task" method="POST">
+														<input name="conteudo" id="conteudo" type="hidden" value="<?= $tarefa->conteudo ?>">
+														<input name="titulo" id="titulo" type="hidden" value="<?= $tarefa->titulo ?>">
+														<label><?= $tarefa->conteudo ?></labe>
+														
+														<button type="submit"> Exluir </button>
+													</form>
+												</div>
+												<section>
+													<form action="/user/home/update_task" method="POST">
+														<input name="conteudo" id="conteudo" type="hidden" value="<?= $tarefa->conteudo ?>">
+														<input name="titulo" id="titulo" type="hidden" value="<?= $tarefa->titulo ?>">
+														<input name="estado" id="estado" type="hidden" value="<?= $tarefa->estado ?>">
+														
+														<button type="submit"><?= $tarefa->estado ?></button>
+													</form>
+												</section>
 											<?php } ?>
 										<?php } ?>
-									</main>
-								</section>
-								<br>
-							<?php } ?>
+									<?php } ?>
+								</main>
+							</section>
+							<br>
 						<?php } ?>
 					</div>
-				</section>
+				</section> 
+				<?php } ?>
 			</main>
 		</div>
 	</body>
